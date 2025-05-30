80768f7e6768bcc6da3a8c57cabf84ff57dcf7d1
Enzo <enzog2106@gmail.com>
2025-05-30 02:32:35 +0200

    cleaned heatmap generation file code

--- New_Full_MdR/New_Full_MdR.srcs/sim_1/new/RO_SIRO_TB.vhd
+++ New_Full_MdR/New_Full_MdR.srcs/sim_1/new/RO_SIRO_TB.vhd
@@ -6,16 +6,14 @@ end RO_SIRO_TB;
 
 architecture Behavioral of RO_SIRO_TB is
     -- Test signals
-    signal enable_ro_s         : STD_LOGIC := '0';
-    signal ro_out_s         : STD_LOGIC;
-    signal enable_siro_s : STD_LOGIC := '0';
+    signal enable_ro_and_siro_s : STD_LOGIC := '0';
+    signal ro_out_s             : STD_LOGIC;
 
     -- Component Declaration
     component RO_SIRO
         Port (
-            enable_ro         : in  STD_LOGIC;
-            ro_out         : out STD_LOGIC;
-            enable_siro : in  STD_LOGIC
+            enable_ro_and_siro  : in  STD_LOGIC;   -- Combined enable signal for both RO and SIRO
+            ro_out              : out STD_LOGIC    -- Outputs from ROs
         );
     end component;
 
@@ -23,25 +21,19 @@ begin
     -- Instantiate Top_Module
     UUT: RO_SIRO
         port map (
-            enable_ro         => enable_ro_s,
-            ro_out         => ro_out_s,
-            enable_siro => enable_siro_s
+            enable_ro_and_siro  => enable_ro_and_siro_s,
+            ro_out              => ro_out_s
         );
 
     -- Stimulus process
     stim_proc: process
     begin
         -- Initial state: both inputs disabled
-        enable_ro_s         <= '0';
-        enable_siro_s <= '0';
+        enable_ro_and_siro_s    <= '0';
         wait for 100 ns;
 
         -- Enable RO
-        enable_ro_s <= '1';
-        wait for 500 ns;
-
-        -- Enable ROattacks
-        enable_siro_s <= '1';
+        enable_ro_and_siro_s    <= '1';
 
         wait;
     end process;
